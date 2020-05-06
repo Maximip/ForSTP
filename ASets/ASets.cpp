bad835aed398240ac7a9894c0d33cc0bdbaec669
Maximip <64906584+Maximip@users.noreply.github.com>
2020-05-06 16:14:58 +0300

    Add files via upload
    Разработать библиотеку обобщенных классов для работы с ассоциативными множествами. В структуру классов входят как минимум:
    IStack<T>: IEnumerable<T> – базовый интерфейс для всех стеков;
    методы:
    void Push(T value);
    void Clear();
    T Pop();
    T Peek();
    свойства:
    int Count;
    bool isEmpty;
    StackException – класс, описывающий исключения, которые могут происходить в ходе работы cо стеком (также можно написать ряд наследников от StackException);
    ArrayStack<T>: IStack<T> – класс стека на основе массива;
    LinkedStack<T>: IStack<T> – класс стека на основе связанного списка;
    IQueue<T>: IEnumerable<T> – базовый интерфейс для всех очередей;
    методы:
    void Enqueue(T value);
    void Clear();
    T Dequeue();
    T Peek();
    свойства:
    int Count;
    bool isEmpty;
    ArrayQueue<T>: IQueue<T> – класс очереди на основе массива;
    LinkedQueue<T>: IQueue<T> – класс очереди на основе связанного списка;
    PriorityQueue<T>: IQueue<T> where T: IComparable – класс очереди с приоритетами объектов;

---
+++ ASets/ASets.cpp
@@ -0,0 +1,90 @@
+﻿
+#include <iostream>
+#include "../../StaticLibForSTP/StaticLibForSTP/A_Sets.h"
+using namespace std;
+using namespace STP;
+//atoi(c)
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	cout << " /////////////////// Стек на основе массива" << endl;
+	ArrayStack AS;
+	int check1 = 0;
+	char ch = 0;
+	while (ch != 'e')
+	{
+		double t;
+		cout << "Введите целое число: ";
+		cin >> t;
+		AS.Push(t);
+		cout << "Если продолжаем дальше, то нажмите любую кнопку, а если всё, то <e>: ";
+		cin >> ch;
+	}
+	AS.End();
+	ch = 0;
+
+	cout << " /////////////////// Стек на основе связанного списка" << endl;
+	LinkedStack LS;
+	int check2 = 0;
+	while (ch != 'e')
+	{
+		double t;
+		cout << "Введите целое число: ";
+		cin >> t;
+		LS.Push(t);
+		cout << "Если продолжаем дальше, то нажмите любую кнопку, а если всё, то <e>: ";
+		cin >> ch;
+	}
+	LS.End();
+	ch = 0;
+
+	cout << " /////////////////// Очередь на основе массива" << endl;
+	ArrayQueue AQ;
+	int check3 = 0;
+	while (ch != 'e')
+	{
+		double t;
+		cout << "Введите целое число: ";
+		cin >> t;
+		AQ.Enqueue(t);
+		cout << "Если продолжаем дальше, то нажмите любую кнопку, а если всё, то <e>: ";
+		cin >> ch;
+	}
+	AQ.End();
+	ch = 0;
+
+	cout << " /////////////////// Очередь на основе связанного списка" << endl;
+	LinkedQueue LQ;
+	int check4 = 0;
+	while (ch != 'e')
+	{
+		double t;
+		cout << "Введите целое число: ";
+		cin >> t;
+		LQ.Enqueue(t);
+		cout << "Если продолжаем дальше, то нажмите любую кнопку, а если всё, то <e>: ";
+		cin >> ch;
+	}
+	LQ.End();
+	ch = 0;
+
+	cout << " /////////////////// Очередь с приоритетом" << endl;
+	PriorityQueue PQ;
+	int check5 = 0;
+	while (ch != 'e')
+	{
+		double t;
+		cout << "Введите целое число: ";
+		cin >> t;
+		PQ.Enqueue(t);
+		cout << "Если продолжаем дальше, то нажмите любую кнопку, а если всё, то <e>: ";
+		cin >> ch;
+	}
+	PQ.End();
+
+	return 0;
+
+
+}
